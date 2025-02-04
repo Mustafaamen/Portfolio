1030779c147188f93875f207b7c8d15aec9f8037
Mustafaamen <mustafaelhadad241@gmail.com>
2025-02-04 22:45:30 +0300

    upload

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -30,7 +30,7 @@ const Contact = () => {
 
       <div className="contact__container container grid">
         <div className="contact__data" data-aos="fade-right">
-          <h3 className="contact__title">Don't be Shy!</h3>
+          <h3 className="contact__title">Don't be Shy !</h3>
           <p className="contact__desc">Talk to me</p>
           <div className="contact__info">
             <div className="info__item border" data-aos="fade-up" data-aos-delay="100">
