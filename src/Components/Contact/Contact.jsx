345ef37151601b5d51cc34a856bd5dd0fdfbf6d6
Mustafaamen <mustafaelhadad241@gmail.com>
2025-02-05 02:20:53 +0300

    upload

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 import {
   FaEnvelopeOpen,
   FaPhoneSquareAlt,
@@ -12,20 +14,26 @@ import { FiSend } from 'react-icons/fi';
 import "./Contact.css";
 
 const Contact = () => {
-
+  useEffect(() => {
+    AOS.init({
+      duration: 1200, // زيادة مدة التأثير لجعلها أبطأ وأكثر سلاسة
+      easing: 'ease-out', // تغيير التوقيت لجعل التأثير أكثر سلاسة
+      once: true, // التأكد من أن التأثير سيحدث مرة واحدة فقط عند التمرير
+    });
+  }, []);
 
   return (
     <section className='contact section'>
-      <h2 className='section__title'>
+      <h2 className='section__title' data-aos="fade-up">
         Get In <span>Touch </span>
       </h2>
 
       <div className="contact__container container grid">
-        <div className="contact__data" >
+        <div className="contact__data" data-aos="fade-right">
           <h3 className="contact__title">Don't be Shy !</h3>
           <p className="contact__desc">Talk to me</p>
           <div className="contact__info">
-            <div className="info__item border" >
+            <div className="info__item border" data-aos="fade-up" data-aos-delay="100">
               <FaEnvelopeOpen className='info__icon' />
               <div>
                 <span className="info__title">Mail Me</span>
@@ -35,7 +43,7 @@ const Contact = () => {
               </div>
             </div>
 
-            <div className="info__item border" >
+            <div className="info__item border" data-aos="fade-up" data-aos-delay="200">
               <FaPhoneSquareAlt className='info__icon' />
               <div>
                 <span className="info__title">Call Me</span>
@@ -45,7 +53,7 @@ const Contact = () => {
               </div>
             </div>
 
-            <div className="info__item border" >
+            <div className="info__item border" data-aos="fade-up" data-aos-delay="300">
               <FaWhatsapp className='info__icon' />
               <div>
                 <span className="info__title">Whatsapp Me</span>
@@ -56,7 +64,7 @@ const Contact = () => {
             </div>
           </div>
 
-          <div className="contact__social" >
+          <div className="contact__social" data-aos="fade-up" data-aos-delay="400">
             <a href="https://www.facebook.com/elhdad20155?locale=ar_AR" className="contact__social__link">
               <FaFacebookF />
             </a>
@@ -72,7 +80,7 @@ const Contact = () => {
           </div>
         </div>
 
-        <form className="coantact__form" >
+        <form className="coantact__form" data-aos="fade-left">
           <div className="form__input__group">
             <div className="form__input__div">
               <input type="text" className="form__control" placeholder="Name" />
